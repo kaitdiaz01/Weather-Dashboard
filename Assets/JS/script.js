69946bc3bdb450b6f426fa52590bd2e1b6bb9a0f
Kaitlyn Diaz <katediaz011@gmail.com>
2022-04-12 16:38:13 -0400

    added functionality to fetch

--- Assets/JS/script.js
+++ Assets/JS/script.js
@@ -2,16 +2,15 @@
 const searchHistory = [];
 // const weatherApiUrl = "https://api.openweathermap.org/";
 const apiKey = "5616ff509464a7cb61e0eff42d696693";
-var cityInput = "";
-
+var cityInput;
 
 //DOM references
-var searchForm = document.querySelector('#search-form');
-var cityInput = document.getElementById("search-input").value;
+var searchForm = document.querySelector("#search-form");
+
 // var todayBox =
-// var forecastBox = 
-// var searchDisplayBox = 
-var submitBtn = document.querySelector('.search-button');
+// var forecastBox =
+// var searchDisplayBox =
+var submitBtn = document.querySelector(".search-button");
 
 //Add timezone for Day.js
 dayjs.extend(window.dayjs_plugin_utc);
@@ -20,29 +19,68 @@ dayjs.extend(window.dayjs_plugin_timezone);
 //function to display search history
 //function displaysearchhistory
 //searchdisplaybox.innerhtml = "";
+var savedCities = [];
+
+function citySubmit(event) {
+  event.preventDefault();
+  cityInput = document.getElementById("search-input").value;
+  weatherData(cityInput);
+
+  // create local storage 
+  // get local storage 
+    // savedCities.push(cityInput);
+
+  //set local storage
 
 
 
-var weatherApiUrl = `http://api.openweathermap.org/geo/1.0/direct?q=London&limit=5&appid=${apiKey}`;
-console.log(weatherApiUrl);
 
-function weatherData(weatherApi) {
 
-    
-    fetch(weatherApiUrl)
-    .then(function(response) {
-        return response.json()
+}
+
+submitBtn.addEventListener("click", citySubmit);
+
+function weatherData(cityInput) {
+  var geoUrl = `http://api.openweathermap.org/geo/1.0/direct?q=${cityInput}}&limit=5&appid=${apiKey}`;
+ 
+  fetch(geoUrl)
+    .then(function (response) {
+      return response.json();
     })
     .then(function (data) {
+        
         console.log(data);
-    })
+        console.log(data[0].lat);
+        var lat = data[0].lat;
+        console.log(data[0].lon);
+        var lon = data[0].lon;
+
+
+        var weatherApiUrl = `https://api.openweathermap.org/data/2.5/onecall?lat=${lat}&lon=${lon}&appid=${apiKey}`
+        fetch(weatherApiUrl)
+        .then(function (response) {
+            //turns response into javascript object notation
+            return response.json();
+        })
+        .then(function (data) {
+            console.log(data);
+        // you use . since these are all objects and not arrays anymore
+        var clouds = data.current.clouds
+        console.log(clouds);
+            
+        })
+    });
     
 }
 
-weatherData(weatherApiUrl);
+// need event listener function to create buttons 
+
+
+
+
+//i can query select the class for the cards which will return an array to run in for loop with i=0
+// weatherData(weatherApiUrl);
 //make a request to the one call and show them howw to make api key
 // fetch coordinates
-// fetch weather 
+// fetch weather
 //functio renderitems(city,data) {}
-
-// submitBtn.addEventListener('click',citySubmit);
